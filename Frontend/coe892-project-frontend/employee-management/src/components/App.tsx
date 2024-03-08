9ca31da7c7d9e9e6b3fd2c86d393b9cac690723d
Co1Sky <cpage0614@gmail.com>
2024-03-08 08:45:18 -0500

    Added Inital Frontend Project

---
+++ Frontend/coe892-project-frontend/employee-management/src/components/App.tsx
@@ -0,0 +1,11 @@
+// import LoginScreen from "./LoginScreen"
+import EmployeeDashboard from "./EmployeeDashboard"
+function App() {
+    return (
+        <div>
+            <EmployeeDashboard />
+        </div>
+    )
+}
+
+export default App
