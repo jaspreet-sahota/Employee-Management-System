b9fa514265da3bce2e88f5cc4c956fa3a59c59af
Co1Sky <cpage0614@gmail.com>
2024-03-22 15:36:00 -0400

    Updated (2024-03-22-3:35pm)

--- Frontend/employee_management/src/main.tsx
+++ Frontend/employee_management/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
-import MainMenu from './MainMenu'
+import MainMenu from './MainMenu/MainMenu'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
