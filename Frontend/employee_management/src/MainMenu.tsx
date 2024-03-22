ff527fb96ce585a483a5bf5fb65892c69cd63cc9
Co1Sky <cpage0614@gmail.com>
2024-03-22 15:34:07 -0400

    Updated (2024-03-22-3:30pm)

---
+++ Frontend/employee_management/src/MainMenu.tsx
@@ -0,0 +1,89 @@
+import React, { FunctionComponent, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button } from "react-bootstrap";
+import "./MainMenu.css";
+
+const MainMenu: FunctionComponent = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Add login logic here using username and password
+  };
+
+  const handleRegister = () => {
+    // Add register logic here using username and password
+  };
+
+  return (
+    <div className="desktop">
+      <main className="dashboard-login">
+        <div className="coe892-project-employee-manage-wrapper">
+          <h1 className="coe892-project-employee-container">
+            <p className="coe892-project">COE892 Project</p>
+            <p className="employee-management-system">
+              Employee Management System
+            </p>
+          </h1>
+        </div>
+        <div className="dashboard-login-inner">
+          <div>
+            <Form className="username">
+              <Form.Control
+                type="text"
+                placeholder="USERNAME"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+            </Form>
+            <Form className="password">
+              <Form.Control
+                type="password"
+                placeholder="PASSWORD"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </Form>
+          </div>
+        </div>
+        <div className="login-register">
+          <div>
+            <Button
+              className="login-button"
+              variant="primary"
+              size="lg"
+              onClick={handleLogin}
+            >
+              LOGIN
+            </Button>
+            <Button
+              className="register-button"
+              variant="primary"
+              size="lg"
+              onClick={handleRegister}
+            >
+              REGISTER
+            </Button>
+          </div>
+        </div>
+
+        <div className="design-members-master">
+          <b className="design-members">
+            Designed By: Colin Page, Jaspreet Sahota, Chris Paul Kumaran, Ahmed
+            Ramadan
+          </b>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default MainMenu;
---
+++ Frontend/employee_management/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './global.css'
+import MainMenu from './MainMenu'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MainMenu />
+  </React.StrictMode>,
+)
