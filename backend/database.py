3b2a4a250c939ad8e079b19d5079dc81a300541a
Jaspreet Sahota <73681817+jaspreet-sahota@users.noreply.github.com>
2024-04-07 18:12:32 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 URL_DATABASE = 'mysql+pymysql://admin:password@employeemanagementsystem-instance-1.ctocskewahid.us-east-2.rds.amazonaws.com:3306/employeemanagementsystem'
 
-engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
