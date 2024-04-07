c151597989c501a6450398f286b4071430acdd70
Jaspreet Sahota <73681817+jaspreet-sahota@users.noreply.github.com>
2024-04-07 18:08:45 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'sqlite:///./EmployeeManagement.db'
+URL_DATABASE = 'mysql+pymysql://admin:password@employeemanagementsystem-instance-1.ctocskewahid.us-east-2.rds.amazonaws.com:3306/employeemanagementsystem'
 
 engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
 
